362ea307fab2412eda6510ca2b70aba2a8b9e7f3
Oksana Kolesnik <os.levina@gmail.com>
2020-02-11 21:04:25 +0200

    Adding Auth

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Auth from './pages/Auth'
+
+function App() {
+  return (
+    <div>
+      <Auth />
+    </div>
+  );
+}
+
+export default App;
