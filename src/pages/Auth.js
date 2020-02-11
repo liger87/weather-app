6a32450346f502e14f43a935e1aa2f23e00d23e0
Oksana Kolesnik <os.levina@gmail.com>
2020-02-11 23:36:23 +0200

    Doing Form

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
+import AuthForm from '../components/AuthForm.js';
 const Auth = props => {
     return (
         <Container className="vh-100">
             <Row className="justify-content-center vh-100">
                 <Col xs="6" className="d-flex align-items-center">
-                    <div className="w-100 text-center">
-                        AUTH FORM
-                    </div>
-                    <div></div>
+                    <AuthForm />
                 </Col>
             </Row>
         </Container>
